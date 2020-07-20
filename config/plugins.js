4ceb473b567cf65dabcfed4c45d886f5da99a502
normal <normal@WarriorShield>
2020-07-20 07:36:32 +0530

    cloudinary hard coding

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
         provider: "cloudinary",
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET')
+          cloud_name: env('officailrj18'),
+          api_key: env('252494111954577'),
+          api_secret: env('A-dApR9BNOpmJVdWF1dwXsU9WIk')
         }
       },
   });
